ba5a225b05e5de582e364e8991ff8bd6c51f6639
Martín Castro <mcastro_20@alu.uabcs.mx>
2023-11-06 14:28:00 -0700

    Ejercicio 4: Rutas con  Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PruebaController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// USUARIO
+Route::get('/user/{name}/{lastName?}', function ($name, $lastName = "doe") {
+    return "<h1>" . "Bienvenido(a): " . $name . " " .$lastName . "</h1>";
+})-> whereAlpha(['name', 'lastName']);
+
+// CALCULADORA
+Route::get('/{calculadora}/{num1}/{num2}', function ($calculadora, $num1, $num2) {
+
+    if($calculadora == 'suma'){
+        return "<h1>". "Resultado: ". $num1 + $num2 ."</h1>";
+    }else if($calculadora == 'resta'){
+        return "<h1>". "Resultado: ". $num1 + $num2 ."</h1>";
+    }else if($calculadora == 'multiplicacion'){
+        return "<h1>". "Resultado: ". $num1 + $num2 ."</h1>";
+    }else if($calculadora == 'division'){
+        return "<h1>". "Resultado: ". $num1 + $num2 ."</h1>";
+    }else{
+        return "<h1>". "error" . "</h1>";
+    }
+    
+})-> where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+// PRUEBA CONTROLADOR
+Route::get('/vista/{name}', function ($name) {
+    return view('prueba ', ['name' => $name]);
+})-> whereAlpha('name');
